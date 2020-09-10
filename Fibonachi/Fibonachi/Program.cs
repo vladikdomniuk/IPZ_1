2446993623d3205d7ade23440a19e3be57f758b4
Vladyslav Domniuk <domnvo.vladik@gmail.com>
2020-09-10 21:58:06 +0300

    Add Fibonachi

---
+++ Fibonachi/Fibonachi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a = 0;
+            int b = 1;
+            int tmp;
+
+            for (int i = 0; i < n; i++)
+            {
+                tmp = a;
+                a = b;
+                b += tmp;
+            }
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
